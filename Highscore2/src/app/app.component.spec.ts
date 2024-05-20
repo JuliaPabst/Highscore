ac549710f46549df86592a56c9eac670bb9c4b0e
Julia Pabst <ju.pabst@gmail.com>
2024-05-20 18:05:18 +0200

    add form fields

--- Highscore2/src/app/app.component.spec.ts
+++ Highscore2/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Highscores'`, () => {
+  it(`should have the 'Highscore2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Highscores');
+    expect(app.title).toEqual('Highscore2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Highscores');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Highscore2');
   });
 });
