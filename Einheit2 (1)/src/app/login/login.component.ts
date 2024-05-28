b48a7c915b17e91fe2ba30ec984fed9827e7b526
Julia Pabst <ju.pabst@gmail.com>
2024-05-28 12:44:24 +0200

    finish exercise 2

--- Einheit2 (1)/src/app/login/login.component.ts
+++ Einheit2 (1)/src/app/login/login.component.ts
@@ -1,34 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormGroup, FormControl, FormBuilder, AbstractControl } from '@angular/forms';
+import {
+  Validators,
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent implements OnInit {
-  email = "";
+  email = '';
+  password = '';
   hide = true;
   isLoading = false;
   loginForm: FormGroup = this.fb.group({
     email: new FormControl(this.email, [
       Validators.required,
-      Validators.minLength(4)
-    ])
+      Validators.minLength(4),
+      Validators.email,
+      Validators.pattern('test@test.at'),
+    ]),
+    password: new FormControl(this.password, [
+      Validators.required,
+      Validators.pattern('12345678'),
+    ]),
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   login() {
     if (this.loginForm.valid) {
       this.isLoading = true;
+      console.log('Login successful.');
+    } else {
+      console.log('Login failed.');
     }
   }
 
-  get form(): { [key: string]: AbstractControl; } {
+  get form(): { [key: string]: AbstractControl } {
     return this.loginForm.controls;
   }
 }
--- Einheit2 (1)/src/app/signup/signup.component.ts
+++ Einheit2 (1)/src/app/signup/signup.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+
 import {
   Validators,
   FormGroup,
   FormControl,
   FormBuilder,
   AbstractControl,
+  ValidationErrors,
+  ValidatorFn,
 } from '@angular/forms';
 
 @Component({
@@ -13,27 +16,76 @@ import {
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  passwordMatchValidator: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    const password1 = control.get('password1');
+    const password2 = control.get('password2');
+
+    return password1 && password2 && password1.value !== password2.value
+      ? { passwordMismatch: true }
+      : null;
+  };
+
   email = '';
+  password1 = '';
+  password2 = '';
   hide = true;
   isLoading = false;
-  signupForm: FormGroup = this.fb.group({
-    email: new FormControl(this.email, [
-      Validators.required,
-      Validators.minLength(4),
-    ]),
-  });
+  signupFailed = false;
+  signupForm: FormGroup = this.fb.group(
+    {
+      email: new FormControl(this.email, [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.email,
+      ]),
+      password1: new FormControl(this.password1, [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      password2: new FormControl(this.password2, [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    },
+    { validators: this.passwordMatchValidator }
+  );
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
 
   login() {
+    const emailControl = this.signupForm.get('email');
+    const password1Control = this.signupForm.get('password1');
+    const password2Control = this.signupForm.get('password2');
+
+    if (
+      emailControl?.errors?.['required'] ||
+      password1Control?.errors?.['required'] ||
+      password2Control?.errors?.['required']
+    ) {
+      this.signupFailed = true;
+    } else {
+      this.signupFailed = false;
+    }
+
     if (this.signupForm.valid) {
       this.isLoading = true;
+      console.log('Login successful.');
+      this.signupFailed = false;
+    } else {
+      console.log('Login failed.');
+      this.signupFailed = true;
     }
   }
 
   get form(): { [key: string]: AbstractControl } {
     return this.signupForm.controls;
+  }
+
+  get passwordMismatch(): boolean {
+    return this.signupForm.hasError('passwordMismatch');
   }
 }
