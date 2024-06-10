39556f8c8660600ea3c8f42f3f3a8c92409a6416
Jules <testadmin@Julias-MacBook-Pro-2.local>
2024-06-10 16:27:02 +0200

    add signup method in backend-service

--- einheit4/src/app/backend.service.ts
+++ einheit4/src/app/backend.service.ts
@@ -13,7 +13,14 @@ export class BackendService {
   };
 
   login(email: string, password: string) {
-    this.http.post<{ Token: string }>('http://localhost:3000/login', { "email": email, "password": password }, this.httpOptions)
+    this.http.post<{ Token: string }>('http://localhost:3000/sessions', { "email": email, "password": password }, this.httpOptions)
+      .subscribe((responseData) => {
+        console.log(responseData.Token);
+      });
+  }
+
+  signup(email: string, password: string, address: string, city: string, zipCode: string) {
+    this.http.post<{ Token: string }>('http://localhost:3000/sessions', { "email": email, "password": password, address: address, city: city, zipCode: zipCode }, this.httpOptions)
       .subscribe((responseData) => {
         console.log(responseData.Token);
       });
